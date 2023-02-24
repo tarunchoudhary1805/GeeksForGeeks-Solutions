93e4a0dd6f105f8bd502e42921d9fbd678b381c3
Tarun Choudhary <57765697+tarunchoudhary1805@users.noreply.github.com>
2023-02-24 14:59:19 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Two numbers with odd occurrences/two-numbers-with-odd-occurrences.java
@@ -0,0 +1,68 @@
+//{ Driver Code Starts
+//Initial Template for Java
+import java.io.*;
+import java.util.*;
+
+class GFG{
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while(t-- > 0)
+        {
+            int N = Integer.parseInt(read.readLine());
+            String input_line[] = read.readLine().trim().split("\\s+");
+            int Arr[]= new int[N];
+            for(int i = 0; i < N; i++)
+                Arr[i] = Integer.parseInt(input_line[i]);
+            
+            Solution ob = new Solution();
+            int[] ans = ob.twoOddNum(Arr,N);
+            for(int i = 0; i < ans.length; i++)
+            {
+                System.out.print(ans[i] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+//User function Template for Java
+class Solution
+{
+    public int[] twoOddNum(int nums[], int N)
+    {
+        // code here
+        HashMap<Integer,Integer> map = new HashMap<>();
+        for(int i=0;i<nums.length;i++){
+            if(map.containsKey(nums[i])){
+                map.put(nums[i],map.get(nums[i])+1);
+            }else{
+                map.put(nums[i],1);
+            }
+        }
+        // System.out.print(map);
+        int arr[] = new int[2];
+        int count = 0;
+          for (Map.Entry<Integer,Integer> entry : map.entrySet()) 
+            {
+                if(entry.getValue() % 2 != 0) {
+                    arr[count++] = entry.getKey();
+                }
+            }
+            Arrays.sort(arr);
+         for(int i=0;i<arr.length/2;i++){
+             swap(arr,i,arr.length-i-1);
+         }
+        return arr;
+    }
+    public static void swap(int [] arr, int i, int j){
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+}
