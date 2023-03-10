de92f3cce6f1beb67bed09202ebaad805493e4e7
Tarun Choudhary <57765697+tarunchoudhary1805@users.noreply.github.com>
2023-03-10 20:37:41 +0530

    Updated Solution - GfG to GitHub

--- Basic/Bit Difference/bit-difference.java
+++ Basic/Bit Difference/bit-difference.java
@@ -14,11 +14,13 @@ class Solution{
     // Function to find number of bits needed to be flipped to convert A to B
     public static int countBitsFlip(int start, int goal){
          int count = 0;
-        while(start != 0 || goal != 0){
-            if((start & 1) != (goal & 1)) count++;
-            start = start >> 1;
-            goal = goal >> 1;
-        }
+         int ans = start ^ goal;
+         while(ans != 0){
+             int a = ans & 1 ;
+             if(a == 1) count++;
+             ans = ans >> 1;
+             
+         }
         return count;
         
     }
