3cb0dd58babba7d22ce9182875f9560ae8ad3f3f
Tarun Choudhary <57765697+tarunchoudhary1805@users.noreply.github.com>
2023-05-12 21:20:27 +0530

    Updated Solution - GfG to GitHub

--- Basic/Count nodes of linked list/count-nodes-of-linked-list.java
+++ Basic/Count nodes of linked list/count-nodes-of-linked-list.java
@@ -25,8 +25,15 @@ class Solution
     //Function to count nodes of a linked list.
     public static int getCount(Node head)
     {
-     if(head == null) return 0;
-     return 1 + getCount(head.next);
+        
+        //Code here
+        Node cur = head;
+        int count = 0;
+        while(cur != null){
+            count++;
+            cur = cur.next;
+        }
+        return count;
     }
 }
     
