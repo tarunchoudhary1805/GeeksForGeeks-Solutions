28710619056ed7e9e85f0d3bd7cb208167dcbae2
Tarun Choudhary <57765697+tarunchoudhary1805@users.noreply.github.com>
2023-05-22 18:10:22 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Non-Repeating Element/non-repeating-element.java
@@ -0,0 +1,68 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+/*package whatever //do not write package name here */
+
+import java.io.*;
+import java.util.*;
+import java.util.Map.Entry;
+
+class GFG {
+	public static void main (String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int testcases = Integer.parseInt(br.readLine());
+		
+		while(testcases-- > 0){
+		    
+		    int n = Integer.parseInt(br.readLine());
+		    int arr[] = new int[n];
+		    
+		    
+		    String line = br.readLine();
+		    String[] elements = line.trim().split(" ");
+		    
+		    for(int index = 0;index < n; index++){
+		        arr[index] = Integer.parseInt(elements[index]);
+		    }
+		    
+		    Check obj = new Check();
+		    
+		    System.out.println(obj.firstNonRepeating(arr,n));
+		        
+		}
+		
+	}
+}
+
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Check{
+    
+    public int firstNonRepeating(int arr[], int n) 
+    { 
+        // Complete the function
+          
+	   
+	   HashMap<Integer,Integer> map = new LinkedHashMap<>();
+	   for(int i=0;i<n;i++){
+	       if(map.containsKey(arr[i])){
+	           map.put(arr[i],map.get(arr[i])+1);
+	       }else{
+	           map.put(arr[i],1);
+	       }
+	   }
+	   for(Map.Entry<Integer,Integer> m : map.entrySet()){
+	       if(m.getValue() == 1){
+	           return m.getKey();
+	       }
+	   }
+	   return -1;
+    }  
+    
+}
+
